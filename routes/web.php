11f58dee329713d3a55f7e0a7228e14954b3b4ce
Albert Melo <albert.uler@brilliantmachine.com.br>
2023-01-11 16:59:25 -0300

    Chutei o balde e iniciei um novo projeto vuetify fora de resources. Agora o frontend será um SPA completamente desvinculado com o Laravel, apenas contido no mesmo repositório

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,13 @@ use Illuminate\Support\Facades\Log;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return $this->render('app');
+    // return Inertia::render('Welcome', [
+    //     'canLogin' => Route::has('login'),
+    //     'canRegister' => Route::has('register'),
+    //     'laravelVersion' => Application::VERSION,
+    //     'phpVersion' => PHP_VERSION,
+    // ]);
 });
 
 Route::get('/dashboard', function () {
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
             refresh: true,
         }),
+        checker({typescript: true})
     ],
 });
